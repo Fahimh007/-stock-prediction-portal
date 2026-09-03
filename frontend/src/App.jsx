c0b3a2e8c45d7be3fbe6a5f27e18651e8dacb3ec
Fahimh007 <fahimhassan67760@gmail.com>
2026-09-04 00:16:03 +0600

    Add accounts and api apps with initial setup; update frontend components and styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import Footer from './components/Footer'
 function App() {
   return (
     <>
-      <h1>Welcome to React</h1>
       <Header />
       <Main />
       <Footer />
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,25 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-        <h1 style={{ color: 'white' }}>Header</h1>
+        <nav className='navbar container pt-3 pb-3 align-items-start'>
+            <Link className='navbar-brand text-light' to="/">Stock Prediction Portal</Link>
+
+            <div>
+              {isLoggedIn ? (
+                <>
+                    <Button text='Dashboard' class="btn-info" url="/dashboard" />
+                    &nbsp;
+                    <button className='btn btn-danger' onClick={handleLogout}>Logout</button>
+                </>
+              ) : (
+                <>
+                    <Button text='Login' class="btn-outline-info" url="/login" />
+                    &nbsp;
+                    <Button text='Register' class="btn-info" url="/register" />
+                </>
+              )}
+            </div>
+        </nav>
     </>
   )
 }
