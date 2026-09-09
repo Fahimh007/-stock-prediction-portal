6082d0585a45705571fdc2f1bb72cafd3c55947e
Fahimh007 <fahimhassan67760@gmail.com>
2026-09-09 16:00:43 +0600

    feat: Add JWT authentication support and create Private/Public route components

--- backend/stock_prediction/settings.py
+++ backend/stock_prediction/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "rest_framework",
+    'rest_framework',
+    'rest_framework_simplejwt',
     'accounts',
     'corsheaders',
     'api',
@@ -101,6 +102,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 
 # Internationalization
